488210718b7368524df53353e40ff8a4c153bd4c
Xanuthatusu <pategeorge12@gmail.com>
2016-06-06 14:39:06 -0600

    Rename files

--- 2.py
+++
@@ -1,5 +0,0 @@
-n = raw_input()
-num = str(n.split(' ')
-
-for i in range(int(num[7])):
-	print '*'*4
---
+++ box.py
@@ -0,0 +1,5 @@
+width = raw_input()
+height = raw_input()
+
+for i in range(int(height)):
+	print '*' * int(width)
